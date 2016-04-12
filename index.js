d37017c89bdbe92ea7dcb06873e84a4f4dc9ec21
JellyKid <seaoftea@gmail.com>
2016-04-12 10:43:34 -0400

    Separating out the callback hell into express middleware

--- index.js
+++ index.js
@@ -5,63 +5,71 @@ var validUrl = require('valid-url');
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(){
-  console.log('db connected');
+  console.log('db shorturl connected');
 });
 
-function redirect(url) {
-
+function setDefaults(req, res, next) {
+  if(/\/(.)/.test(req.path)){
+    req.parsedPath = /\/(.+)/.exec(req.path)[1];
+    req.shortUrlPath = req.protocol + '://' + req.hostname + req.baseUrl +'/';
+    return next();
+  } else {
+    res.status(404).send('No path found!');
+  }
 }
 
+function checkUrlDb(req,res,next){
+    model.findOne({URL: req.parsedPath},function(err, existingUrl){
+      if(err){
+        return next(new Error(err));
+      }
+      if(existingUrl){
+        res.send({
+          original_url: req.parsedPath,
+          short_url: req.shortUrlPath + existingUrl.id
+        });
+      } else {
+        next();
+      }
+    });
+}
 
-module.exports = function(req,res){
-  if(req.path === '/'){
-    res.send('Insert directions here');
-    return;
-  }
-  var path = /^\/(.+)/.exec(req.path)[1];
-  model.findOne({URL: path},function(err, existingUrl){ //find url already in database
+function checkShortDb(req, res, next) {
+  model.findById(req.parsedPath,function(err, existingShortUrl){
     if(err){
-      console.log(err);
-      return err;
+      return next(new Error(err));
     }
-    if(existingUrl){
-      res.send({
-        original_url: path,
-        short_url: "http://" + req.get('host') + "/short/" + existingUrl.id
-      });
+    if(existingShortUrl){
+      res.redirect(302, existingShortUrl.URL);
     } else {
-      model.findById(path,function(err, existingShortUrl){ //find shortcode
-        if(err){
-          console.log(err);
-          return err;
-        }
-        if(existingShortUrl){
-          res.redirect(302, existingShortUrl.URL);
-        } else {
-          if(validUrl.isUri(path)){
-            var newShortURL = new model({URL: path});
-            newShortURL.save(function(err,short){
-              if(err){
-                res.send({
-                  request: path,
-                  error: err
-                });
-              } else {
-                res.send({
-                  original_url: path,
-                  short_url: "http://" + req.get('host') + "/short/" + short.id
-                });
-              }
-            });
-
-          } else {
-            res.send({
-              request: path,
-              error: "Invalid URL or shortcode"
-            });
-          }
-        }
-      });
+      next();
     }
   });
+}
+
+function newURl(req, res, next){
+  if(validUrl.isUri(req.parsedPath)){
+    var newShortURL = new model({URL: req.parsedPath});
+    newShortURL.save(function(err,short){
+      if(err){
+        return next(new Error(err));
+      }
+      res.send({
+        original_url: req.parsedPath,
+        short_url: req.shortUrlPath + short.id
+      });
+    });
+  } else {
+    res.send({
+      request: req.parsedPath,
+      error: "Invalid URL or shortcode"
+    });
+  }
+}
+
+module.exports = {
+  setDefaults: setDefaults,
+  checkUrlDb: checkUrlDb,
+  checkShortDb: checkShortDb,
+  newURl: newURl
 };
